cff756a9ef4b9cfaf8a4cf654896f92dbc732433
jonnydaw <jonathandaw29@gmail.com>
2025-03-13 23:11:06 +0000

    allowed host

--- androidApis/androidApis/settings.py
+++ androidApis/androidApis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mz_f@tu0ri$qg=694i3xj--_ljnmg@i9upvwt_s=ut-aen+b3c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://moodymusic.pythonanywhere.com"]
 
 
 # Application definition
